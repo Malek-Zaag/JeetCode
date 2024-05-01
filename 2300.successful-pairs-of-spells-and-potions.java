60b6226bb40a1579d8431cd7b3f1372b638801ad
Malek <zaag.malek1@gmail.com>
2024-05-01 12:06:51 +0200

    sliding window

---
+++ 2300.successful-pairs-of-spells-and-potions.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=2300 lang=java
+ *
+ * [2300] Successful Pairs of Spells and Potions
+ */
+
+// @lc code=start
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        
+    }
+}
+// @lc code=end
+
