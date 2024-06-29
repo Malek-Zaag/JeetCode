15b4c0958894902b6cfd2cd042b875f0751945d5
Malek <zaag.malek1@gmail.com>
2024-06-29 17:45:42 +0200

    Asteroid Collision

--- 735.asteroid-collision.java
+++ 735.asteroid-collision.java
@@ -7,25 +7,28 @@
 // @lc code=start
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Deque;
+import java.util.stream.Collector;
+import java.util.stream.IntStream;
 
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        Deque<Integer> st = new ArrayDeque<>();
-        st.add(asteroids[0]);
-        for (int i = 1; i < asteroids.length; i++) {
-            if (asteroids[i] > st.peek() && asteroids[i] > 0 && st.peek() < 0) {
-                continue;
-            } else if (asteroids[i] == -st.peek()) {
-                st.pop();
-            } else if (asteroids[i] > 0 && st.peek() > 0) {
-                st.add(asteroids[i]);
+        int n = asteroids.length;
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            while (j > 0 && asteroids[i] < 0 && asteroids[j - 1] > 0
+                    && Math.abs(asteroids[i]) > asteroids[j - 1]) {
+                j--;
             }
+            if (j == 0 || asteroids[i] > 0 || asteroids[j - 1] < 0) {
+                asteroids[j] = asteroids[i];
+                j++;
+            } else if (Math.abs(asteroids[i]) == Math.abs(asteroids[j - 1]))
+                j--;
         }
-        int[] res = new int[st.size()];
-        System.arraycopy(st, 0, res, 0, st.size());
-        return res;
+        return IntStream.range(0, j).map(k -> asteroids[k]).toArray();
     }
 }
 // @lc code=end
